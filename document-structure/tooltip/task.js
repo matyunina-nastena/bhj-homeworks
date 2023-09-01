f888f0d48f3fe5f23dbfe0342df09e48ea067d6f
Matyunina-nastya <matyunina-nastena@mail.ru>
2023-09-01 20:09:32 +0300

    Исправленная версия

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,27 +1,35 @@
 const tooltips = [...document.querySelectorAll(".has-tooltip")];
+const div = document.createElement('div'); 
 
 tooltips.forEach(tooltip => {
     tooltip.addEventListener('click', (e) => {
         e.preventDefault();
 
         // Добавление подсказки
-        const div = document.createElement('div');
-        div.classList.add('tooltip');
+        div.classList.add('tooltip'); 
         div.textContent = tooltip.getAttribute('title');
         div.classList.add('tooltip_active');
         
         // Закрытие подсказки при повторном клике на ссылку
         const tooltipActive = tooltip.querySelector('.tooltip_active');
         if (tooltipActive) {
-            tooltipActive.remove('tooltip_active');
-            return
-        };
-        // Запрет на открытие более 1-ой активной подсказки
-        const openedTooltips = document.querySelectorAll('.tooltip');
-        if (openedTooltips.length >= 1) {
+            tooltipActive.remove();
+            return;
+        }
+
+    // Запрет на открытие более 1-ой активной подсказки
+    const openedTooltips = document.querySelectorAll('.tooltip');
+    if (openedTooltips.length >= 1) {
         openedTooltips[0].remove('tooltip_active');
-    }
-        tooltip.appendChild(div);
-    });
+    } 
+
+    const coordinates = tooltip.getBoundingClientRect();
+    const coordinatesTop = coordinates.top + window.scrollY + coordinates.height;
+    const coordinatesLeft = coordinates.left + window.scrollX;
+    div.style.top = coordinatesTop + 'px';
+    div.style.left = coordinatesLeft + 'px';
+    
+    document.body.appendChild(div);
 });
+})
 
